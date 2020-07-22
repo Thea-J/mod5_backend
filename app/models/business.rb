681c6a3bef630bf0c350c35cb97a881ccb82b22b
github username <github email address>
2020-07-22 02:36:18 +0100

    reseeded

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,7 +2,7 @@ class Business < ApplicationRecord
     belongs_to :business_owner
 
     @retail_img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR0iRx2b74xgtLAithy7wdJJyQAHkYaTrhGhw&usqp=CAU"
-    @hospitality_img = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.clock-software.com%2Fimages%2Fself-service%2Fscene-3.png&imgrefurl=https%3A%2F%2Fwww.clock-software.com%2Fhotel-self-service-app.html&tbnid=rdx8Intlh08s1M&vet=12ahUKEwiw6tnRtNnqAhVY0YUKHYxgDFsQMygUegUIARDLAQ..i&docid=3qIlwEg8-klIFM&w=395&h=279&q=hotel%20check%20in%20cartoon&safe=strict&ved=2ahUKEwiw6tnRtNnqAhVY0YUKHYxgDFsQMygUegUIARDLAQ"
+    @hospitality_img = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.epicentrofestival.com%2Fwp-content%2Fuploads%2F2020%2F02%2Fepicentrofestival-emoji-hyatt-hotel-hospitality-industry-gastronomy-leader-cartoon-i3a5xynjol-720x720.jpg&imgrefurl=https%3A%2F%2Fwww.epicentrofestival.com%2F394502c8-epicentrofestival-emoji-hyatt-hotel-hospitality-industry-gastronomy-leader-cartoon-i3a5x.html&tbnid=FhPbuT1X5aIUXM&vet=12ahUKEwiakNGv0t_qAhVMNRoKHeT6DsIQMyg5egQIARA1..i&docid=J-jSOEaiKn-YDM&w=720&h=720&q=hospitality%20cartoon&hl=en-GB&ved=2ahUKEwiakNGv0t_qAhVMNRoKHeT6DsIQMyg5egQIARA1"
     @food_drinks_img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ2jUfh-jnXeqScQnDPLQreBm6v2Z5eZ9qBqA&usqp=CAU"
     @it_img = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTyzR-tMDcfCwJQc9lfrwa9zpsEYoON4LnpBw&usqp=CAU"
     @beauty_img ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSVENQedJgrVxRlstQDhQaCJORxA_l43gTKeQ&usqp=CAU"
